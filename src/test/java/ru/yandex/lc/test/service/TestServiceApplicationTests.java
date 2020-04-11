396e46b550ee87e9b08d2b5dee7f6626b95a4265
Denis Tsyplakov <denis.tsyplakov@gmail.com>
2020-04-11 12:53:06 +0300

    end to end test added

--- src/test/java/ru/yandex/lc/test/service/TestServiceApplicationTests.java
+++ src/test/java/ru/yandex/lc/test/service/TestServiceApplicationTests.java
@@ -1,13 +1,32 @@
 package ru.yandex.lc.test.service;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TestServiceApplicationTests {
+
+	@Autowired
+	private TestRestTemplate template;
+
+	@Autowired
+	AddController addController;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testAddControllerMath() {
+		Assertions.assertEquals(3, addController.add(1, 2));
+	}
+
+	@Test
+	void testAddControllerHttp() {
+		Assertions.assertEquals(5, template.getForObject("/add?a=2&b=3", Integer.class));
 	}
 
 }
